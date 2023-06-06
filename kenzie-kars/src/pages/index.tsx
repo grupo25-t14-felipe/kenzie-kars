2797619fdb3da1a0a50a3bb5c3033c86bc044c47
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-06-06 11:39:59 -0400

    Add: create base styled

--- kenzie-kars/src/pages/index.tsx
+++ kenzie-kars/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1>Olá</h1>
+      
     </main>
   );
 }
