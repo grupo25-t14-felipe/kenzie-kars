fed406fb54b791680ca96c6671804c3acb44408d
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-06-06 13:44:38 -0400

    Add: create input styled

--- kenzie-kars/src/pages/_app.tsx
+++ kenzie-kars/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import '@/styles/typography.css'
 import '@/styles/buttons.css'
+import '@/styles/inputs.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
