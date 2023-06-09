cda61ac1860687e53a9632c886bd7942c17d7786
Gabriel-Dev <gabr.lima7@gmail.com>
2023-06-09 14:43:09 -0300

    feat: announcement page layout

--- kenzie-kars/src/components/profile.tsx
+++ kenzie-kars/src/components/profile.tsx
@@ -1,6 +1,6 @@
 const Profile = ({ name }: any) => {
   return (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center gap-4 font-semibold">
       <span className="bg-brand-1 w-8 h-8 rounded-full text-grey-10 text-center relative">
         <p className="relative top-1 heading-7-500">{name.split(' ').map((letter:string) => letter.charAt(0))}</p>
       </span>
