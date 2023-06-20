9147685eddd0890a35c38cf91edfbf86cb4c9ae4
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-06-20 11:03:19 -0300

    feat: CSS dos filtros

--- kenzie-kars/src/components/filters.tsx
+++ kenzie-kars/src/components/filters.tsx
@@ -19,181 +19,181 @@ const Filters = () => {
   };
 
   return (
-    <aside className="flex flex-col gap-4 sm:w-full md:w-[25%] p-8">
+    <aside className="flex flex-col justify-start gap-4 sm:w-full md:w-[25%] p-8">
       <h3 className="font-semibold text-lg">Marca</h3>
-      <span>
-        <p
+      <div className="flex flex-col gap-1 justify-start w-full pr-[250px]">
+        <button
           className={`text-sm text-grey-2 ${brand === "General Motors" && "selected"}`}
           onClick={() => setBrand("General Motors")}>
           General Motors
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${brand === "Fiat" && "selected"}`}
           onClick={() => setBrand("Fiat")}>
           Fiat
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${brand === "Ford" && "selected"}`}
           onClick={() => setBrand("Ford")}>
           Ford
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${brand === "Honda" && "selected"}`}
           onClick={() => setBrand("Honda")}>
           Honda
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${brand === "Toyota" && "selected"}`}
           onClick={() => setBrand("Toyota")}>
           Toyota
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${brand === "Volkswagen" && "selected"}`}
           onClick={() => setBrand("Volkswagen")}>
           Volkswagen
-        </p>
-      </span>
+        </button>
+      </div>
 
       <h3 className="font-semibold text-lg">Modelo</h3>
-      <span>
-        <p
+      <div className="flex flex-col gap-1 justify-start pr-[250px]">
+        <button
           className={`text-sm text-grey-2 ${model === "Civic" && "selected"}`}
           onClick={() => setModel("Civic")}>
           Civic
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Corolla" && "selected"}`}
           onClick={() => setModel("Corolla")}>
           Corolla
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Cruze" && "selected"}`}
           onClick={() => setModel("Cruze")}>
           Cruze
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Fit" && "selected"}`}
           onClick={() => setModel("Fit")}>
           Fit
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Gol" && "selected"}`}
           onClick={() => setModel("Gol")}>
           Gol
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Ka" && "selected"}`}
           onClick={() => setModel("Ka")}>
           Ka
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Onix" && "selected"}`}
           onClick={() => setModel("Onix")}>
           Onix
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${model === "Pulse" && "selected"}`}
           onClick={() => setModel("Pulse")}>
           Pulse
-        </p>
-      </span>
+        </button>
+      </div>
 
       <h3 className="font-semibold text-lg">Cor</h3>
-      <span>
-        <p
+      <div className="flex flex-col pr-[250px]">
+        <button
           className={`text-sm text-grey-2 ${color === "Azul" && "selected"}`}
           onClick={() => setColor("Azul")}>
           Azul
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${color === "Branca" && "selected"}`}
           onClick={() => setColor("Branca")}>
           Branca
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${color === "Cinza" && "selected"}`}
           onClick={() => setColor("Cinza")}>
           Cinza
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${color === "Prata" && "selected"}`}
           onClick={() => setColor("Prata")}>
           Prata
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${color === "Preta" && "selected"}`}
           onClick={() => setColor("Preta")}>
           Preta
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${color === "Verde" && "selected"}`}
           onClick={() => setColor("Verde")}>
           Verde
-        </p>
-      </span>
+        </button>
+      </div>
 
       <h3 className="font-semibold text-lg">Ano</h3>
-      <span>
-        <p
+      <div className="flex flex-col gap-1 justify-start pr-[250px]">
+        <button
           className={`text-sm text-grey-2 ${year === "2022" && "selected"}`}
           onClick={() => setYear("2022")}>
           2022
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2021" && "selected"}`}
           onClick={() => setYear("2021")}>
           2021
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2018" && "selected"}`}
           onClick={() => setYear("2018")}>
           2018
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2015" && "selected"}`}
           onClick={() => setYear("2015")}>
           2015
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2013" && "selected"}`}
           onClick={() => setYear("2013")}>
           2013
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2012" && "selected"}`}
           onClick={() => setYear("2012")}>
           2012
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${year === "2010" && "selected"}`}
           onClick={() => setYear("2010")}>
           2010
-        </p>
-      </span>
+        </button>
+      </div>
 
       <h3 className="font-semibold text-lg">Combustível</h3>
-      <span>
-        <p
+      <div className="flex flex-col gap-1 justify-start pr-[250px]">
+        <button
           className={`text-sm text-grey-2 ${fuel === "Diesel" && "selected"}`}
           onClick={() => setFuel("Diesel")}>
           Diesel
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${fuel === "Gasolina" && "selected"}`}
           onClick={() => setFuel("Gasolina")}>
           Gasolina
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${fuel === "Etanol" && "selected"}`}
           onClick={() => setFuel("Etanol")}>
           Etanol
-        </p>
-        <p
+        </button>
+        <button
           className={`text-sm text-grey-2 ${fuel === "Flex" && "selected"}`}
           onClick={() => setFuel("Flex")}>
           Flex
-        </p>
-      </span>
+        </button>
+      </div>
 
       <h3 className="font-semibold text-lg">Km</h3>
       <div className="flex gap-4">
