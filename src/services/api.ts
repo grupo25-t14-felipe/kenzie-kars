9905c29a15a2f1e1a1512c3c7eeea7981ac24c3d
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-07-06 16:17:00 -0300

    fix: api backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://kenzie-kars-api.onrender.com",/
-  baseURL: "http://localhost:3001",
-  // timeout: 20000
+  baseURL: "https://kenzie-kars-api.onrender.com",
+  timeout: 20000
 });
 
 export default api;
