a2e37927a1ef5c6ca5f6eaebe0bf07277e9d89cc
miranda-Leonardo <leonardo.miranda.3@hotmail.com>
2023-07-06 14:53:30 -0400

    Fix: register announcements

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kenzie-kars-api.onrender.com",
-  timeout: 20000
+  // baseURL: "https://kenzie-kars-api.onrender.com",/
+  baseURL: "http://localhost:3001",
+  // timeout: 20000
 });
 
 export default api;
